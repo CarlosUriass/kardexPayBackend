aa2b8969cdfbb4c9a4aa3f2d82116df7c402c11a
Carlos Urias <hicarlosurias@gmail.com>
2025-05-17 21:08:09 -0700

    feat: app and server clases to start the express aplication

---
+++ src/presentation/server.ts
@@ -0,0 +1,34 @@
+import express, { Router } from "express";
+import cors from "cors";
+
+interface Options {
+  port: number;
+  routes: Router;
+}
+
+export class Server {
+  private app = express();
+  private readonly port: number;
+  private readonly routes: Router;
+
+  constructor(options: Options) {
+    const { port, routes } = options;
+    this.port = port;
+    this.routes = routes;
+  }
+
+  private middlewares() {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  async start() {
+    this.middlewares();
+
+    this.app.use(this.routes);
+
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en el puerto ${this.port}`);
+    });
+  }
+}
